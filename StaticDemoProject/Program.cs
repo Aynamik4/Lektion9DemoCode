de9df0b9c684c011064c58aafc910fb789b8c8a5
Aynamik4 <violintailgut@gmail.com>
2020-09-24 13:10:14 +0200

    First Commit

---
+++ StaticDemoProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StaticDemoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StaticDemoClass.StaticProp = 10;
+            StaticDemoClass.StaticMethod();
+
+            StaticDemoClass myInstance = new StaticDemoClass();
+            myInstance.InstanceProp = 20;
+            myInstance.InstanceMethod();
+
+            double sqr = Math.Sqrt(9);
+
+            Random r = new Random(1);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{r.Next(1, 10 + 1)} ");
+            }
+        }
+    }
+}
---
+++ StaticDemoProject/StaticDemoClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticDemoProject
+{
+    class StaticDemoClass
+    {
+        public static int StaticProp { get; set; }
+        public int InstanceProp { get; set; }
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine("Static Method!");
+            Console.WriteLine(StaticProp);
+            //Console.WriteLine(InstanceProp);
+            //InstanceMethod();
+        }
+
+        public void InstanceMethod()
+        {
+            Console.WriteLine("Instance Method");
+            Console.WriteLine(StaticProp);
+            Console.WriteLine(InstanceProp);
+            StaticMethod();
+        }
+    }
+}
